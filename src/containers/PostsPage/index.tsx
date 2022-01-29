c3b0d4eb327e5b465903654153d2044b01287057
Alexey Popov <alexnn0987@mail.ru>
2022-01-29 11:22:08 +0300

    react-redux

--- src/containers/PostsPage/index.tsx
+++ src/containers/PostsPage/index.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import PostsList from "../../components/PostsLists";
 import data from "../../assets/data";
+import { fetchPostsDataAction } from "../../store/actions";
 
 const PostsPage: React.FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setTimeout(() => dispatch(fetchPostsDataAction(data)), 5000);
+  }, []);
+
   return <PostsList data={data} />;
 };
 
---
+++ src/store/reducer.ts
@@ -0,0 +1,22 @@
+// import { bindActionCreators } from "redux";
+
+import { PostsActionsType } from "./actions";
+import { ActionsType, ReducerType } from "./types";
+
+const initialState: ReducerType = {
+  posts: [],
+};
+
+const reduser = (state = initialState, action: ActionsType): ReducerType => {
+  switch (action.type) {
+    case PostsActionsType.fetchPostsData:
+      return {
+        ...state,
+        posts: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reduser;
