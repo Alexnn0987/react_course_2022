48fbe7c613be374b24fb2e3d1e35404459c57342
Alexey Popov <alexnn0987@mail.ru>
2022-02-07 23:41:09 +0300

    request for all posts

--- src/containers/OnePostPage/index.tsx
+++ src/containers/OnePostPage/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useParams, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import OnePost from "../../components/OnePost";
-import data from "../../assets/data";
+import { getPostsData } from "../../store/Posts/selectors";
 
 const OnePostPage: React.FC = () => {
   const { id } = useParams();
+  const data = useSelector(getPostsData);
   const post = data.find((el) => el.id === +id!);
   if (post) {
     return <OnePost post={post} />;
   }
-  return <Navigate to="/posts" />;
+  return <div>404</div>;
 };
 
 export default OnePostPage;
